e207649e6f43eb62c23f02db7cca6c58e46ab3da
Ty <tyla.boff@outlook.com>
2024-06-05 18:57:37 +0100

    more change

--- app1/modules/app1/main.tf
+++ app1/modules/app1/main.tf
@@ -1,1 +1,11 @@
 resource "random_pet" "main" {}
+
+terraform {
+  required_version = ">= 1.7.5"
+  required_providers {
+    random = {
+      source = "hashicor/random"
+      version = ">= 0.0"
+    }
+  }
+}
